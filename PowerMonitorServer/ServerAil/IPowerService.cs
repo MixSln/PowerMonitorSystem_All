8065e1bf682827b8bc7391ba13bc7b2f20b0b434
MixSln <ycm99871@sina.com>
2015-04-11 22:43:09 +0800

    refresh

--- PowerMonitorServer/ServerAil/IPowerService.cs
+++ PowerMonitorServer/ServerAil/IPowerService.cs
@@ -14,49 +14,87 @@ namespace PowerServer.ServerAil
         /// </summary>
         /// <param name="username">用户名</param>
         /// <param name="passwd">密码</param>
-        /// <returns></returns>
+        /// <returns>返回用户ID</returns>
         [OperationContract]
-        bool userLogin(string username,string passwd);
+        int userLogin(string username,string passwd);
 
         #region Room Operations
+        //增加一个配电室信息
         [OperationContract]
         int roomAdd(RoomBaseInfo room);
+        //将一个配电室与一个用户管理
         [OperationContract]
         bool roomAddToUser(int userid, int roomid);
+        //删除一个配电室，以及其与所有用户和设备的关联
         [OperationContract]
         bool roomDel(int roomid);
+        //删除一个配电室与一个用户的管理
         [OperationContract]
         bool roomDelFromUser(int userid, int roomid);
+        //更新一个配电室的基本信息
         [OperationContract]
-        bool roomUpdate(RoomBaseInfo room);
+        bool roomBaseInfoUpdate(RoomBaseInfo room);
+        //获取一个配电室的基本信息
         [OperationContract]
-        RoomBaseInfo roomGet(int roomid);
+        RoomBaseInfo roomBaseInfoGet(int roomid);
+        //获取某个用户名下某个配电室的详细信息（包括其中的设备）
+        [OperationContract]
+        RoomInfo roomInfoGet(int userid, int roomid);
+        //获取某个用户名下的配电室列表
+        Dictionary<int, RoomInfo> roomInfoOfUserGet(int userid);
+        //获取所有配电室列表
+        Dictionary<int, RoomInfo> roomInfoAllGet();
         #endregion
 
         #region Device Operations
+        //添加一个设备
         [OperationContract]
         int deviceAdd(DeviceBaseInfo device);
+        //添加一个设备到一个配电室
         [OperationContract]
         bool deviceAddToRoom(int roomid, DeviceBaseInfo device);
+        //删除一个设备
         [OperationContract]
         bool deviceDel(int deviceid);
+        //从一个配电室删除一个设备
         [OperationContract]
         bool deviceDelFromRoom(int roomid, int deviceid);
+        //更新一个设备
         [OperationContract]
-        bool deviceUpdate(DeviceBaseInfo device);
+        bool deviceBaseInfoUpdate(DeviceBaseInfo device);
+        //获取一个设备的基本信息
         [OperationContract]
-        DeviceBaseInfo deviceGet(int deviceid);
+        DeviceBaseInfo deviceBaseInfoGet(int deviceid);
+        //获取一个设备的历史数据
+        [OperationContract]
+        List<DeviceRealTimeInfo> deviceHistoryGet(int deviceid);
+        //获取一个配电室的设备列表
+        [OperationContract]
+        Dictionary<int, DeviceBaseInfo> deviceBaseInfoOfRoomGet(int roomid);
+        //获取一个用户的所有设备列表
+        [OperationContract]
+        Dictionary<int, DeviceBaseInfo> deviceBaseInfoOfUserGet(int userid);
+        //获取所有设备列表
+        [OperationContract]
+        Dictionary<int, DeviceBaseInfo> deviceBaseInfoAllGet();
         #endregion
 
         #region UserOperations
+        //添加用户，并获得添加后用户的id，以便继续添加配电室（以及可能设备）
         [OperationContract]
         int userAdd(UserBaseInfo user);
+        //删除一个用户及其所有配电室和设备关联信息
         [OperationContract]
         bool userDel(int userid);
+        //更新一个用户本身的基本信息（不涉及配电室和设备）
         [OperationContract]
-        bool userUpdate(UserBaseInfo user);
+        bool userBaseInfoUpdate(UserBaseInfo user);
+        //获取一个用户的基本信息(不包含配电室和设备)
         [OperationContract]
-        UserBaseInfo userGet(int userid);
+        UserBaseInfo userBaseInfoGet(int userid);
+        //获取当前所有用户列表
+        [OperationContract]
+        Dictionary<int, UserBaseInfo> userBaseInfoAllGet();
         #endregion
     }
 
--- PowerMonitorServer/ServerBll/PowerService.cs
+++ PowerMonitorServer/ServerBll/PowerService.cs
@@ -4,6 +4,7 @@ using System.ServiceModel;
 using System.Text;
 using PowerServer.ServerAil;
 using PowerServer.ServerBll.IModules;
+using PowerServer.DataContracts;
 
 namespace PowerServer.ServerBll
 {
@@ -51,11 +52,116 @@ namespace PowerServer.ServerBll
         #endregion
 
         #region Implement Interface
-        public bool userLogin(string username, string passwd)
+        public int userLogin(string username, string passwd)
         {
             
+            return 0;
+        }
+
+        #region 配电室
+        public int roomAdd(RoomBaseInfo room)
+        {
+            return 0;
+        }
+        public bool roomAddToUser(int userid, int roomid)
+        {
             return false;
         }
+        public bool roomDel(int roomid)
+        {
+            return false;
+        }
+        public bool roomDelFromUser(int userid, int roomid)
+        {
+            return false;
+        }
+        public bool roomBaseInfoUpdate(RoomBaseInfo room)
+        {
+            return false;
+        }
+        public RoomBaseInfo roomBaseInfoGet(int roomid)
+        {
+            return new RoomBaseInfo();
+        }
+        public RoomInfo roomInfoGet(int userid, int roomid)
+        {
+            return new RoomInfo();
+        }
+        public Dictionary<int, RoomInfo> roomInfoOfUserGet(int userid)
+        {
+            return new Dictionary<int, RoomInfo>();
+        }
+        public Dictionary<int, RoomInfo> roomInfoAllGet()
+        {
+            return new Dictionary<int, RoomInfo>();
+        }
+        #endregion
+
+        #region 设备
+        public int deviceAdd(DeviceBaseInfo device)
+        {
+            return 0;
+        }
+        public bool deviceAddToRoom(int roomid, DeviceBaseInfo device)
+        {
+            return false;
+        }
+        public bool deviceDel(int deviceid)
+        {
+            return false;
+        }
+        public bool deviceDelFromRoom(int roomid, int deviceid)
+        {
+            return false;
+        }
+        public bool deviceBaseInfoUpdate(DeviceBaseInfo device)
+        {
+            return false;
+        }
+        public DeviceBaseInfo deviceBaseInfoGet(int deviceid)
+        {
+            return new DeviceBaseInfo();
+        }
+        public List<DeviceRealTimeInfo> deviceHistoryGet(int deviceid)
+        {
+            return new List<DeviceRealTimeInfo>();
+        }
+        public Dictionary<int, DeviceBaseInfo> deviceBaseInfoOfRoomGet(int roomid)
+        {
+            return new Dictionary<int, DeviceBaseInfo>();
+        }
+        public Dictionary<int, DeviceBaseInfo> deviceBaseInfoOfUserGet(int userid)
+        {
+            return new Dictionary<int, DeviceBaseInfo>();
+        }
+        public Dictionary<int, DeviceBaseInfo> deviceBaseInfoAllGet()
+        {
+            return new Dictionary<int, DeviceBaseInfo>();
+        }
+        #endregion
+
+        #region 用户
+        public int userAdd(UserBaseInfo user)
+        {
+            return 0;
+        }
+        public bool userDel(int userid)
+        {
+            return false;
+        }
+        public bool userBaseInfoUpdate(UserBaseInfo user)
+        {
+            return false;
+        }
+        public UserBaseInfo userBaseInfoGet(int userid)
+        {
+            return new UserBaseInfo();
+        }
+        public Dictionary<int, UserBaseInfo> userBaseInfoAllGet()
+        {
+            return new Dictionary<int, UserBaseInfo>();
+        }
+        #endregion
         #endregion
     }
 }
